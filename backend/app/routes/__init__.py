3b5920a7ea5f64223e96ac4ec82f58b121057a03
Felipe <lipem61@gmail.com>
2024-08-25 22:15:43 -0300

    Dockeradicionado e sistema rodando localmente

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,6 +1,16 @@
+from flask import Blueprint
+
+# Importação dos outros blueprints
 from .auth import auth_bp
 from .user import user_bp
 from .collection_point import collection_point_bp
 
+# Criação de um blueprint para a rota raiz
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def index():
+    return "Backend is running!"
+
 # Adicione os blueprints no pacote routes
-__all__ = ['auth_bp', 'user_bp', 'collection_point_bp']
+__all__ = ['auth_bp', 'user_bp', 'collection_point_bp', 'main_bp']
