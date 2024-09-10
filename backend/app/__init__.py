3524cb869af938b70dd2203e6cab06ea199d8264
Felipe <lipem61@gmail.com>
2024-09-10 09:36:43 -0300

    correção de bugs no login

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS  # Importar o CORS
 from .config import Config
 
 db = SQLAlchemy()
@@ -11,6 +12,9 @@ jwt = JWTManager()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Configuração do CORS
+    CORS(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
